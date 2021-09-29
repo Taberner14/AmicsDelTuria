db294e6e8279a5c502c7f2246a6b125b00f7f7d1
Taberner14 <60652737+Taberner14@users.noreply.github.com>
2021-09-29 10:56:56 +0200

    Subida archivos

---
+++ php/P2/phpInfo.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * * Descripción: phpInfo
+ * *
+ * * Descripción extensa: Iremos añadiendo cosas complejas en PHP.
+ * *
+ * * @author  Carlos Hernández al386007@uji.es
+ * * @copyright 2021 Carlos
+ * * @license http://www.fsf.org/licensing/licenses/gpl.txt GPL 2 or later
+ * * @version 1
+ * */
+
+phpinfo();
+?>
