7e49b8d1198d0794ea44e7eb019cd005d5a32aa3
Carlos <al386007@uji.es>
2021-10-05 13:20:27 +0200

    P2_F

---
+++ php/P2_F/partials/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--**
+ * * Descripción: Cabecera portal Programa Aprender PHP
+ * *
+ * * Descripción extensa: Pagina web dividida en 4 ficheros.
+ * *
+ * * @author  Lola <dllido@uji.es>
+ * * @version 1
+ * * @link http://dllido@al.nisu.org/P0/holaMundo.php
+ * * -->
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Bienvenido a nuestra Web</title>
+	<META name="Author" content="AlumnoXXX">
+	<link rel="stylesheet" href="../css/estilo.css" type="text/css">
+	
+
+</head>
+
+
+<body>
+	<header>
+		<img id="logo" src="../img/logo.png" id="logo" alt="logo" />
+		<p id="eslogan">Taberner Muñoz: La dupla de oro</p>
+	</header>
