c20325bfa0c6baf439cd3b63df8ca5a8de61172d
Carlos <al386007@uji.es>
2021-10-08 12:53:26 +0200

    Tal

--- php/P2_F/includes/registrar.php
+++ php/P2_F/includes/registrar.php
@@ -15,11 +15,8 @@ function handler($pdo,$table)
     echo $query;
     try { 
         $a=array($_REQUEST['nombre'], $_REQUEST['descripcion'], $_REQUEST['localizacion'], $_REQUEST['distancia']);
-        echo $a;
         $consult = $pdo->prepare($query);
-        echo $consult;
         $a=$consult->execute(array($_REQUEST['nombre'], $_REQUEST['descripcion'], $_REQUEST['localizacion'], $_REQUEST['distancia'] ));
-        echo $a;
         if (1>$a)echo "InCorrecto";
     } catch (PDOExeption $e) {
         echo ($e->getMessage());
